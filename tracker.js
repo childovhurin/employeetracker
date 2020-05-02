9ac2df10fc7e31f95f96065d4f92ca2e331d039f
Megan Murphy <childovhurin@gmail.com>
2020-05-02 18:24:10 -0400

    start function works

--- tracker.js
+++ tracker.js
@@ -10,3 +10,33 @@ const connection = mysql.createConnection({
     password: "",
     database: "employee_trackerDB"
 });
+//connection and start function
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
+});
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "start",
+                choices: ["VIEW", "ADD", "UPDATE", "DELETE", "EXIT PROGRAM"]
+            }
+        ]).then(function (answer) {
+            //this function calls the next inquirer function based on user's choice
+            if (answer.start === "VIEW") {
+                viewQuestions();
+            } else if (answer.start === "ADD") {
+                addQuestions();
+            } else if (answer.start === "UPDATE") {
+                updateQuestions();
+            } else if (answer.start === "DELETE") {
+                deleteQuestions();
+            } else {
+                process.exit();
+            }
+        })
+}
